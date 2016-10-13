57e751efe7d0c738cdbde8bcda2cf707f9277dee
Aaron Lipinski <kris.lipinski@gmail.com>
2016-10-13 20:10:18 +1300

    move todo item to class

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mobx = require('mobx')
-const { el, mount } = require('redom')
+const { el, mount, setChildren, list } = require('redom')
 
 const todos = mobx.observable([
   {text: 'make a todo list', done: false},
@@ -22,24 +22,34 @@ function onchange (e) {
   todos[e.target.dataset.idx].done = e.target.checked
 }
 
-const todoItem = (todo, i) =>
-  el('li', {id: i},
-    el('label',
-      el('input', {
-        type: 'checkbox',
-        'data-idx': i,
-        checked: todo.done,
-        onchange
-      }),
-      todo.text,
-      el('button', {onclick: () => remove(i)}, 'X')
+
+class TodoItem {
+  constructor () {
+    this.el = el('li')
+  }
+
+  update (todo, i) {
+    setChildren(this.el, [
+      el('label',
+        el('input', {
+          type: 'checkbox',
+          'data-idx': i,
+          checked: todo.done,
+          onchange
+        }),
+        todo.text,
+        el('button', {onclick: () => remove(i)}, 'X')
+      )]
     )
-  )
+  }
+}
+
+const ti = list('ul', TodoItem)
 
 const todoApp =
   el('div',
     el('h3', 'TODO'),
-    el('ul', ...todos.map(todoItem)),
+    ti,
     el('form', {onsubmit: add},
       el('input', {name: 'todo'}),
       el('button', 'Add')
@@ -48,5 +58,8 @@ const todoApp =
 
 mount(document.body, todoApp)
 
+ti.update(todos)
+
+window.ti = ti
 window.mobx = mobx
 window.todos = todos
